e4ccf6cda41019851498b1a22a42f3982ed4b28f
Naveen K <129737291+arkNaveen@users.noreply.github.com>
2023-09-29 23:43:51 +0530

    Update parameters.h

--- parameters.h
+++ parameters.h
@@ -73,16 +73,22 @@ void ultrasound(){
   // eff_distance is the water level measured
 
 }
-// ON the buzzer and turn the switches according to the water level
+//and turn the switches according to the water level
 void waterlevelcheck(){
   if(eff_distance < 10) {
+      // ON the buzzer for 1 second when water is low 
     servo1.write(90);
     digitalWrite(param.buzzPin,HIGH);
+    delay(1000);
+    digitalWrite(param.buzzPin,LOW);
   }
   // If water is more witch is triggered bore motor is turned OFF with servo 
   if(eff_distance > 1900) {
+       // OFF the buzzer for 5 second when water is low 
     servo1.write(0);
     digitalWrite(param.buzzPin,HIGH);
+    delay(5000);
+    digitalWrite(param.buzzPin,LOW);
   }
 }
 void rainwatercheck(){
@@ -93,6 +99,7 @@ void rainwatercheck(){
     Serial.print("Servo on switch to ON Rain Harvest Mode");
     // This turns on the top lid to collect rain water
     switchInterrupt2();
+      
   }
 }
 
