d07f65f3bdb7e83a09a77cd23dd23d03b763d5d9
Naveen K <129737291+arkNaveen@users.noreply.github.com>
2023-09-29 22:33:05 +0530

    The main project cpp code done in Arduino IDE

---
+++ main.ino
@@ -0,0 +1,61 @@
+#include "parameters.h"
+void setup() {
+  // Pin setup for servo pins
+  servosetup();
+
+  // Pin setup for Interrupt attached Switches
+  switchsetup();
+  
+  // Pin setup for Ultrasonic Sensor
+  ultrasoundsetup();
+
+  // Pin setup for 
+  pinMode(param.moisturePin,INPUT);
+  Serial.begin(9600);
+  
+}
+void loop() {
+  // Measure the water level in the tank
+  ultrasound();
+
+  // Conditions of the water level supply -> Reflected on servo motors
+  waterlevelcheck();
+
+  // Checking for availabilty of Rain through moisture sensor
+  rainwatercheck();
+
+  // Checking for availabilty of Bluetooth connection
+  if (Serial.available())
+    {   
+        param.BT_input = Serial.readString();   // read input string from bluetooth 
+        
+        if(eff_distance<10)
+        {
+          Serial.print("Critical water level");
+        }
+        
+        if(eff_distance>1900)
+        {
+          Serial.print("Excess water level");
+        }
+        // "A" input from android APP returns the water level
+        if(param.BT_input=="A")
+        {
+          Serial.print("Current water level ");
+          Serial.print(eff_distance);
+        }
+        // "B" input from android APP stops the servo motor and sets the default BT input to A
+        if(param.BT_input=="B")
+        {
+          Serial.print("You have choosen to stop the motor)");
+          servo1.write(0);
+          param.BT_input = "A";
+        }
+      }
+  if(param.BT_input=="A")
+  {
+    servo1.write(0);
+  }
+  delay(500);
+}
+
